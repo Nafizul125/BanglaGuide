fd14b603c8fb56281debe9a6cce383738c3386df
Nafizul125 <107269962+Nafizul125@users.noreply.github.com>
2025-08-28 03:12:38 +0600

    Delete hotels directory

--- hotels/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Hotel
-
-class HotelForm(forms.ModelForm):
-    class Meta:
-        model = Hotel
-        fields = ['name', 'location', 'phone_number', 'room_rent', 'facilities', 'rating', 'image']
